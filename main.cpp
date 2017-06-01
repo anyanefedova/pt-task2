a0130ad1e43574bde449c8a41bb41179a0693087
Anya Nefedova <Anechka155@gmail.com>
2017-06-01 11:10:12 +0400

    modify the constuctor class child

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ public:
 class Child: public Base
 {
 public:
-    Child() { cout << "Construct Child object\n"; }
+    Child() { cout << "Construct new Child object\n"; }
     ~Child() { cout << "Destruct Child object\n"; }
 
     virtual void func() const { cout << "Function func() Now call in Child class\n"; }
